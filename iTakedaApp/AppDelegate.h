28aa80e83e56774d2bc06ad7e43e7450cc1c82a3
tingting.sheng <user001@tmd.local>
2017-05-08 19:04:23 +0800

    Initial Commit

---
+++ iTakedaApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTakedaApp
+//
+//  Created by user001 on 2017/5/8.
+//  Copyright © 2017年 stt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
