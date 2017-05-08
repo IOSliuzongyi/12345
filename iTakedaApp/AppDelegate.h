8faf47a7c863dd4e6f8e08395516529a6a7be831
lzy <liuzongyiemail@sina.com>
2017-05-08 21:47:48 +0800

    “firstCommit”

--- iTakedaApp/AppDelegate.h
+++ iTakedaApp/AppDelegate.h
@@ -7,11 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
-
+//#import "SAICHTTPRequest.h"
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+//@property (strong, nonatomic) UIWindow *window;
+//
+//@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+//@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+//@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+//@property (strong,nonatomic) SAICHTTPRequest* httpRequest;
 
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
 
